3d5daa426db67ba008ed364736bb81a232dbb9f3
Allan <allan.bernier1@gmail.com>
2024-04-11 17:14:54 +0200

    get params

--- NODE/first-app/index.js
+++ NODE/first-app/index.js
@@ -1,22 +1,24 @@
 const express = require('express');
-
 const app = express();
 
 app.listen(3000, () => {
   console.log('App running on port 3000');
 });
 
+// localhost:3000/
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-/*
+// localhost:3000/hello/tata
+app.get("/hello/:name", (req, res) => {
+  const name = req.params.name 
+  res.send(`Hello ${name}`);
+})
 
-$npm init
-$npm i nodemon express
-
-  "scripts": {
-    "dev": "nodemon index.js",
-  },
-
-  */
+// localhost:3000/params?name=toto
+app.get("/param", (req, res) => {
+  const name = req.query.name
+  const lastname = req.query.lastname
+  res.send(`Hello ${name} ${lastname}`);
+})
